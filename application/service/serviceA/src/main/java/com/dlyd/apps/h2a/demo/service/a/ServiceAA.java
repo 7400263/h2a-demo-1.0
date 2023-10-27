7e6551581d6904db62a030db165ad8187d5c0e5e
sunxin <7400263@qq.com>
2023-10-27 15:50:12 +0800

    install platform jars

--- application/service/serviceA/src/main/java/com/dlyd/apps/h2a/demo/service/a/ServiceAA.java
+++ application/service/serviceA/src/main/java/com/dlyd/apps/h2a/demo/service/a/ServiceAA.java
@@ -1,7 +1,9 @@
 package com.dlyd.apps.h2a.demo.service.a;
+import org.noc.hccp.platform.common.msg.PlatformMessage;
 
 public class ServiceAA {
     public int add(int a, int b) {
+        PlatformMessage pm = null;
         return a+b;
     }
 }
